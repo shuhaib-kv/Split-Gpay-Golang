4c0d93124aa62225714e0244f9db79bfa9077ac1
shuhaib <kvshuhaib2@gmail.com>
2023-02-08 15:54:44 +0530

    initialized database

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shuhaib-kv/Split-Gpay-Golang.git/pkg/routes"
+)
 
+var g = gin.Default()
+
+func main() {
+	routes.RoutesOfApi(g)
 }
