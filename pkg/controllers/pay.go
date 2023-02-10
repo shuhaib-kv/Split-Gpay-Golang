d131d05f97cd064bea8ad9fec335990edb00d97b
shuhaib-kv <kvshuhaib2@gmail.com>
2023-02-10 11:44:16 +0530

    added payment

--- pkg/controllers/pay.go
+++ pkg/controllers/pay.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func PaySplit(c *gin.Context) {
 	var body struct {
@@ -8,6 +12,17 @@ func PaySplit(c *gin.Context) {
 		expenceid uint
 		splitid   uint
 	}
-	c.BindJSON(&body)
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(http.StatusConflict, gin.H{
+			"status": false,
+			"error":  "Invalid JSON",
+			"data":   "null",
+		})
+		return
+	}
+
+	c.JSON(http.StatusFound, gin.H{
+		"": "",
+	})
 
 }
